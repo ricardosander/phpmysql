e6bf29d6b13fd8ae70230e506f901debe7eabcd0
Ricardo Sander <ricardo.sander.lopes@gmail.com>
2016-11-04 00:24:24 -0200

    Evitando sql-injection e problemas com caracteres especiais.

--- banco-produto.php
+++ banco-produto.php
@@ -3,11 +3,19 @@ include("conecta.php");
 
 function insereProduto($conexao, $nome, $preco, $descricao, $categoria_id, $usado) {
 
+    $nome = mysqli_real_escape_string($conexao, $nome);
+    $descricao = mysqli_real_escape_string($conexao, $descricao);
+    $preco = mysqli_real_escape_string($conexao, $preco);
+    $categoria_id = mysqli_real_escape_string($conexao, $categoria_id);
+    $usado = mysqli_real_escape_string($conexao, $usado);
+
     $query = "insert into produtos (nome, preco, descricao, categoria_id, usado) values ('{$nome}', {$preco}, '{$descricao}', {$categoria_id}, $usado)";
     return mysqli_query($conexao, $query);
 }
 
 function buscaProduto($conexao, $id) {
+
+    $id = mysqli_real_escape_string($conexao, $id);
 
 	$query = "select * from produtos where id = {$id}";
 	$resultado = mysqli_query($conexao, $query);
@@ -16,12 +24,20 @@ function buscaProduto($conexao, $id) {
 
 function alteraProduto($conexao, $id, $nome, $preco, $descricao, $categoria_id, $usado) {
 
+    $id = mysqli_real_escape_string($conexao, $id);
+    $nome = mysqli_real_escape_string($conexao, $nome);
+    $descricao = mysqli_real_escape_string($conexao, $descricao);
+    $preco = mysqli_real_escape_string($conexao, $preco);
+    $categoria_id = mysqli_real_escape_string($conexao, $categoria_id);
+    $usado = mysqli_real_escape_string($conexao, $usado);
+
 	$query = "update produtos set nome = '{$nome}', preco = {$preco}, descricao = '{$descricao}', categoria_id = {$categoria_id}, usado = {$usado} where id = {$id}";
 	return mysqli_query($conexao, $query);
 }
 
 function removerProduto($conexao, $id) {
 
+    $id = mysqli_real_escape_string($conexao, $id);
     $query = "delete from produtos where id = {$id}";
     return mysqli_query($conexao, $query);
 }
--- banco-usuario.php
+++ banco-usuario.php
@@ -2,6 +2,9 @@
 include("conecta.php");
 function buscaUsuario($conexao, $email, $senha) {
 
+    $email = mysqli_real_escape_string($conexao, $email);
+    $senha = mysqli_real_escape_string($conexao, $senha);
+
     $senha = md5($senha);
     $query = "select * from usuarios where email = '{$email}' and senha = '{$senha}'";
     $resultado = mysqli_query($conexao, $query);
