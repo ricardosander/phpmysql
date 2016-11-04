2ee31860be6bd1f73cb93024d3229cc62f92aeb8
Ricardo Sander <ricardo.sander.lopes@gmail.com>
2016-11-04 00:37:58 -0200

    Enviando e-mail pelo php.

---
+++ envia-contato.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+
+require_once("PHPMailerAutoload.php");
+
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$mensagem = $_POST['mensagem'];
+
+$usuario = "";
+$senha = "";
+
+
+$mail = new PHPMailer();
+$mail->isSMTP();
+$mail->Host = 'smtp.gmail.com';
+$mail->Port = 587;
+$mail->SMTPSecure = 'tls';
+$mail->SMTPAuth = true;
+$mail->Username = $usuario;
+$mail->Password = $senha;
+$mail->setFrom($usuario, "Site do Ricardo");
+$mail->addAddress($usuario);
+$mail->Subject = "Email de contato da loja";
+$mail->msgHTML("<html>de: {$nome}<br/>email: {$email}<br/>mensagem: {$mensagem}</html>");
+$mail->AltBody = "de: {$nome}\nemail:{$email}\nmensagem: {$mensagem}";
+if($mail->send()) {
+    $_SESSION["success"] = "Mensagem enviada com sucesso";
+    header("Location: index.php");
+} else {
+    $_SESSION["danger"] = "Erro ao enviar mensagem " . $mail->ErrorInfo;
+    header("Location: contato.php");
+}
+die();
